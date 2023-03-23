36131cff348f09afeb74d3eef39b8f65736673a2
STY_Games <86984992+STYGames@users.noreply.github.com>
2023-03-22 19:02:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const icon = document.querySelector('.toggle-switch-icon:first-child i');
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    icon.classList.replace('fa-sun', 'fa-moon');
+    icon.classList.replace('fa-moon', 'fa-sun');
     localStorage.setItem('theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    icon.classList.replace('fa-moon', 'fa-sun');
+    icon.classList.replace('fa-sun', 'fa-moon');
     localStorage.setItem('theme', 'light');
   }
 }
@@ -21,6 +21,11 @@ if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
+    icon.classList.replace('fa-moon', 'fa-sun');
+  } else if (currentTheme === 'light') {
+    toggleSwitch.checked = false;
     icon.classList.replace('fa-sun', 'fa-moon');
   }
 }
+
+//Buscador
